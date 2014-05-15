8840538000264dcb53cff2f14efa2450d235765f
Andrew Peekema <apeekema@gmail.com>
2014-05-15 14:30:52 -0700

    main.m file with the intent of the project

---
+++ main.m
@@ -0,0 +1,11 @@
+% Derive, simulate, plot, and animate one leg of ATRIAS
+% Author: Andrew Peekema
+
+% Cleanup
+clc       % Clear the command prompt
+clear all % Remove all workspace variables
+close all % Close all figure windows
+
+display('Solving the equations of motion...')
+
+% TODO: Solve the kinematics
