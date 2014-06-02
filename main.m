68d2f7ef2caa021420fb467952ae3dd53adbcb50
Andrew Peekema <apeekema@gmail.com>
2014-06-02 15:42:59 -0700

    Testing the created control equation

--- main.m
+++ main.m
@@ -19,5 +19,46 @@ eqs = dynamicEqns(k,z);
 eqs = feedbackLinearization(k,eqs);
 
 % Convert the control torques to c code
-tauA = ccode(eqs(1));
-tauB = ccode(eqs(2));
+%tauA = ccode(eqs(1));
+%tauB = ccode(eqs(2));
+tauA = matlabFunction(eqs(1));
+tauB = matlabFunction(eqs(2));
+
+% Declare constants
+g = 9.81; % m/s^2
+% Link 1 and 5
+r1 = 0.5;  % length (m)
+% Link 2 and 4
+r2 = 0.5; % length (m)
+% Mass of ATRIAS
+m = 59.9; % kg
+% Rotor inertia
+I = 0.0019; % kg*m^2
+% Motor inertia (as seen by the output)
+I3 = 50^2*I;
+I6 = I3;
+% Motor damping (as seen by the output)
+c3 = 19; % N*s/rad
+c6 = c3;
+% Spring parameters
+ks = 1600; % N/rad
+cS = 1.49; % N*s/rad
+
+% Test the functions
+FxDes = 0;
+FyDes = 0;%-m*g;
+k1_11 = 0;
+k1_22 = 0;
+k2_11 = 0;
+k2_22 = 0;
+dq1 = 0;
+dq2 = 0;
+dq3 = 0;
+dq6 = 0;
+q1 = pi/4;
+q2 = pi/2;
+q3 = 3*pi/4;
+q6 = pi/4;
+
+testA = tauA(FxDes,FyDes,I3,c3,cS,dq1,dq2,dq3,dq6,g,k1_11,k2_11,ks,m,q1,q2,q3,q6,r1,r2);
+testB = tauB(FxDes,FyDes,I3,c3,cS,dq1,dq2,dq3,dq6,g,k1_22,k2_22,ks,m,q1,q2,q3,q6,r1,r2);
