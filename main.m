662c0e3708b9dc7eb4f0e10a5273240533affaac
Andrew Peekema <apeekema@gmail.com>
2014-06-03 15:47:05 -0700

    Testing force control inputs

--- main.m
+++ main.m
@@ -49,26 +49,26 @@ cS = 1.49; % N*s/rad
 % Test the functions
 FxDes = 0;
 FyDes = 0;
-k1_11 = 1;
-k1_22 = 1;
-k2_11 = 1;
-k2_22 = 1;
+k1_11 = 10;
+k1_22 = 10;
+k2_11 = k1_11^2;
+k2_22 = k1_22^2;
 dq1 = 0;
 dq2 = 0;
 dq3 = 0;
 dq6 = 0;
-q1 = pi/4;
-q2 = pi/2;
-q3 = 3*pi/4;
-q6 = pi/4;
+q1 = 1.0677;
+q2 = 1.04389;
+q3 = 2.11163;
+q6 = 1.067738;
 % Insert some error into the desired angles
-q3des = q3+0.1;
-q6des = q6-0.1;
+%q3des = q3+0.1;
+%q6des = q6-0.1;
 
 % Torques for force control
-%testA = tauA(FxDes,FyDes,I3,c3,cS,dq1,dq2,dq3,dq6,g,k1_11,k2_11,ks,m,q1,q2,q3,q6,r1,r2)
-%testB = tauB(FxDes,FyDes,I6,c6,cS,dq1,dq2,dq3,dq6,g,k1_22,k2_22,ks,m,q1,q2,q3,q6,r1,r2)
+testA = tauA(FxDes,FyDes,I3,c3,cS,dq1,dq2,dq3,dq6,g,k1_11,k2_11,ks,m,q1,q2,q3,q6,r1,r2)
+testB = tauB(FxDes,FyDes,I6,c6,cS,dq1,dq2,dq3,dq6,g,k1_22,k2_22,ks,m,q1,q2,q3,q6,r1,r2)
 
 % Torques for position control
-testA = tauA(I3,c3,cS,dq1,dq2,dq3,k1_11,k2_11,ks,q1,q2,q3,q3des)
-testB = tauB(I6,c6,cS,dq1,dq6,k1_22,k2_22,ks,q1,q6,q6des)
+%testA = tauA(I3,c3,cS,dq1,dq2,dq3,k1_11,k2_11,ks,q1,q2,q3,q3des)
+%testB = tauB(I6,c6,cS,dq1,dq6,k1_22,k2_22,ks,q1,q6,q6des)
